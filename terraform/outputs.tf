86c13f1360c94442cfd8fe955d239353dbd2f380
Alexssander Fonseca <alex.marques.fa@gmail.com>
2026-01-01 16:20:12 -0300

    feat: ajustes

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,9 +1,9 @@
 output "sam_artifacts_bucket_name" {
-  description = "Name of the S3 bucket for SAM artifacts"
+  description = "The name of the S3 bucket for storing SAM artifacts."
   value       = aws_s3_bucket.sam_artifacts.bucket
 }
 
 output "lambda_exec_role_arn" {
-  description = "ARN of the Lambda execution IAM role"
+  description = "The ARN of the IAM role for the Lambda function."
   value       = aws_iam_role.lambda_exec_role.arn
 }
