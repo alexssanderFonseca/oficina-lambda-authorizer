65166b41b63b0727ae2f2c7b1e95acc82fcbd662
Alexssander Fonseca <alex.marques.fa@gmail.com>
2026-01-03 14:38:03 -0300

    feat: ajustes

--- terraform/main.tf
+++ terraform/main.tf
@@ -106,28 +106,22 @@ data "aws_api_gateway_rest_api" "existing_api" {
   name = var.existing_api_gateway_name
 }
 
-resource "aws_api_gateway_resource" "clientes" {
+resource "aws_api_gateway_resource" "login_clientes" {
   rest_api_id = data.aws_api_gateway_rest_api.existing_api.id
   parent_id   = data.aws_api_gateway_rest_api.existing_api.root_resource_id
-  path_part   = "clientes"
-}
-
-resource "aws_api_gateway_resource" "login" {
-  rest_api_id = data.aws_api_gateway_rest_api.existing_api.id
-  parent_id   = aws_api_gateway_resource.clientes.id
-  path_part   = "login"
+  path_part   = "login-clientes"
 }
 
 resource "aws_api_gateway_method" "post" {
   rest_api_id   = data.aws_api_gateway_rest_api.existing_api.id
-  resource_id   = aws_api_gateway_resource.login.id
+  resource_id   = aws_api_gateway_resource.login_clientes.id
   http_method   = "POST"
   authorization = "NONE"
 }
 
 resource "aws_api_gateway_integration" "lambda" {
   rest_api_id             = data.aws_api_gateway_rest_api.existing_api.id
-  resource_id             = aws_api_gateway_resource.login.id
+  resource_id             = aws_api_gateway_resource.login_clientes.id
   http_method             = aws_api_gateway_method.post.http_method
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
@@ -140,7 +134,7 @@ resource "aws_lambda_permission" "apigw" {
   function_name = aws_lambda_function.authorizer.function_name
   principal     = "apigateway.amazonaws.com"
 
-  source_arn = "${data.aws_api_gateway_rest_api.existing_api.execution_arn}/*/${aws_api_gateway_method.post.http_method}${aws_api_gateway_resource.login.path}"
+  source_arn = "${data.aws_api_gateway_rest_api.existing_api.execution_arn}/*/${aws_api_gateway_method.post.http_method}${aws_api_gateway_resource.login_clientes.path}"
 }
 
 resource "aws_api_gateway_deployment" "deploy" {
@@ -148,7 +142,7 @@ resource "aws_api_gateway_deployment" "deploy" {
   
   triggers = {
     redeployment = sha1(jsonencode([
-      aws_api_gateway_resource.login.id,
+      aws_api_gateway_resource.login_clientes.id,
       aws_api_gateway_method.post.id,
       aws_api_gateway_integration.lambda.id,
     ]))
